4c90c529dde4e442eb522192eba72f05ef0308b6
hoou <1084502012@qq.com>
2020-06-20 11:27:31 +0800

    update conftest

--- conftest.py
+++ conftest.py
@@ -11,9 +11,12 @@ import pytest
 import allure
 from py._xmlgen import html
 from selenium import webdriver
+from common.readconfig import ini
 from config.conf import SCREENSHOT_DIR
-from tools.times import datetime_strftime
 from common.inspect import inspect_element
+from tools.send_mail import send_report
+from tools.times import datetime_strftime, timestamp
+
 driver = None
 
 
@@ -53,7 +56,6 @@ def pytest_runtest_makereport(item):
                 extra.append(pytest_html.extras.html(html))
         report.extra = extra
         report.description = str(item.function.__doc__)
-        report.nodeid = report.nodeid.encode("utf-8").decode("unicode_escape")
 
 
 @pytest.mark.optionalhook
@@ -75,6 +77,41 @@ def pytest_html_results_table_html(report, data):
     if report.passed:
         del data[:]
         data.append(html.div('通过的用例未捕获日志输出.', class_='empty log'))
+
+
+@pytest.mark.optionalhook
+def pytest_html_report_title(report):
+    report.title = "pytest示例项目测试报告"
+
+
+@pytest.mark.optionalhook
+def pytest_configure(config):
+    config._metadata.clear()
+    config._metadata['测试项目'] = "测试百度官网搜索"
+    config._metadata['测试地址'] = ini.url
+
+
+@pytest.mark.optionalhook
+def pytest_html_results_summary(prefix, summary, postfix):
+    # prefix.clear() # 清空summary中的内容
+    prefix.extend([html.p("所属部门: XX公司测试部")])
+    prefix.extend([html.p("测试执行人: 随风挥手")])
+
+
+def pytest_terminal_summary(terminalreporter, exitstatus, config):
+    """收集测试结果"""
+    result = {
+        "total": terminalreporter._numcollected,
+        'passed': len(terminalreporter.stats.get('passed', [])),
+        'failed': len(terminalreporter.stats.get('failed', [])),
+        'error': len(terminalreporter.stats.get('error', [])),
+        'skipped': len(terminalreporter.stats.get('skipped', [])),
+        # terminalreporter._sessionstarttime 会话开始时间
+        'total times': timestamp() - terminalreporter._sessionstarttime
+    }
+    print(result)
+    if result['failed'] or result['error']:
+        send_report()
 
 
 def _capture_screenshot():
